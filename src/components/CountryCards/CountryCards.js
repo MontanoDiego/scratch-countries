8620fb1932b7f01e004d81edbeeaace26e38f75d
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-18 14:45:18 -0800

    creating react functional components for later!

---
+++ src/components/CountryCards/CountryCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CountryCards.css';
+
+export default function CountryCards() {
+  return (
+    <div className='cardContainer'>
+      <p>This is where the cards go!</p>
+    </div>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CountryCards from '../CountryCards/CountryCards.js';
+import Search from '../Search/Search.js';
 
 export default function Main() {
   return (
     <>
       <h1>Countries of the World!</h1>
+      <Search />
+      <CountryCards />
     </>
   );
 }
