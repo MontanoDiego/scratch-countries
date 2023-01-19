9e0ac62c7d0fe9348359c1d298fc74fbad2bdb8d
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-18 16:52:28 -0800

    selector options mapped and are setting option value

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CountryCards from '../CountryCards/CountryCards.js';
 import Search from '../Search/Search.js';
 import { useCountries } from '../../hooks/useCountries.js';
@@ -7,10 +7,12 @@ import './Main.css';
 export default function Main() {
   const countries = useCountries();
 
+  const [filter, setFilter] = useState('');
+
   return (
     <>
       <h1>Countries of the World!</h1>
-      <Search />
+      <Search filter={filter} />
       <div className='countries-container'>
         {countries.map((country) => (
           <CountryCards key={country.id} {...country} />
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCountries } from '../../hooks/useCountries.js';
 import './Search.css';
 
-export default function Search() {
+export default function Search({ filter, setFilter }) {
+  const countries = useCountries();
+
+  const [option, setOption] = useState('All');
+
+  const continents = ['All', 'North America', 'Africa', 'Asia', 'Oceania', 'South America', 'Antarctica', 'Europe'];
+
+  const handleFilterSelect = (e) => {
+    setOption(e.target.value);
+  };
+
   return (
-    <div>This is where the search options go!</div>
+    <div>
+      <select value={option} onChange={handleFilterSelect} >
+        {continents.map((continent) => (
+          <option key={continent} value={continent} > {continent} </option>
+        ))}
+      </select>
+    </div>
   );
 }
